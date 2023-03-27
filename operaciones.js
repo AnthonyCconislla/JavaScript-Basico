2271a5b882cd1af562f1a50e55a84bb42b13fc7f
AnthonyCconislla <tonyccm2@gmail.com>
2023-03-27 13:57:21 -0500

    operaciones basicas

---
+++ operaciones.js
@@ -0,0 +1,22 @@
+// suma directa
+console.log(9 + 5)
+
+// constantes
+const a = 9
+const b = 5
+
+// suma con constantes
+const sum = a + b
+console.log(sum)
+
+// resta con constantes
+const res = a - b
+console.log(res)
+
+// producto con constantes
+const mul = a * b
+console.log(mul)
+
+// división con constantes
+const div = a / b
+console.log(div)
